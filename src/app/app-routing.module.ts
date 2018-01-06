dbe4d13c2590e0a4edf69a974d97c1c33fbad5a9
homahi <homahi0128@gmail.com>
2018-01-06 20:53:26 +0900

    7-1-4/7-1-5:RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'detail',
+        component: DetailComponent
+      }
+    ]
   }
 ];
 
