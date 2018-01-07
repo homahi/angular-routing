185230bdeb73751ccba44e725f202699ce9bc461
homahi <homahi0128@gmail.com>
2018-01-07 10:31:21 +0900

    7-9-2:AuthGuardに失敗した場合は'/'にルーティングする

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of'
@@ -6,9 +7,16 @@ import 'rxjs/add/operator/delay'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private route: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return Observable.of(true).delay(3000);
+    if (Math.random() <= 0.5) {
+      return true;
+    } else {
+      alert('アクセスできません');
+      this.route.navigate(['/']);
+      return false;
+    }
   }
 }
